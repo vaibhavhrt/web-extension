5693cacc74b72b778eafe5ca08b85bade5f91fe1
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-05-15 18:15:04 +0530

    add completion item provider for browserAction

--- src/completionItemProviders.ts
+++ src/completionItemProviders.ts
@@ -1,14 +1,17 @@
 import chromeCompletionItemProvider from './chrome/chromeCompletionItemProvider';
-import accessibilityFeaturesCompletionItemProvider from './chrome/accessibilityFeatures/accessibilityFeaturesCompletionItemProvider';
-import alarmsCompletionItemProvider from './chrome/alarms/alarmsCompletionItemProvider';
+import accessibilityFeaturesCompletionItemProvider from './chrome/accessibilityFeatures';
+import alarmsCompletionItemProvider from './chrome/alarms';
 import bookmarksCompletionItemProvider from './chrome/bookmarks';
+import browserActionCompletionItemProvider from './chrome/browserAction';
 
-import tabsCompletionItemProvider from './chrome/tabs/tabsCompletionItemProvider';
+import tabsCompletionItemProvider from './chrome/tabs';
 
 export default [
     chromeCompletionItemProvider,
     accessibilityFeaturesCompletionItemProvider,
     alarmsCompletionItemProvider,
     bookmarksCompletionItemProvider,
+    browserActionCompletionItemProvider,
+
     tabsCompletionItemProvider,
 ];
