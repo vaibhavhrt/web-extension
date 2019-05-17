2d8c9940f5d38d6a55cc5081a949d3fde7d44fce
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-05-17 16:54:26 +0530

    add completion for debugger api

--- src/completionItemProviders.ts
+++ src/completionItemProviders.ts
@@ -10,6 +10,7 @@ import commandsCompletionItemProvider from './chrome/stable/commands';
 import contentSettingsCompletionItemProvider from './chrome/stable/contentSettings';
 import contextMenusCompletionItemProvider from './chrome/stable/contextMenus';
 import cookiesCompletionItemProvider from './chrome/stable/cookies';
+import debuggerCompletionItemProvider from './chrome/stable/debugger';
 
 import tabsCompletionItemProvider from './chrome/stable/tabs';
 
@@ -26,6 +27,7 @@ export default [
     contentSettingsCompletionItemProvider,
     contextMenusCompletionItemProvider,
     cookiesCompletionItemProvider,
+    debuggerCompletionItemProvider,
 
     tabsCompletionItemProvider,
 ];
